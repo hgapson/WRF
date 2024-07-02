15128454254c92a208e57f3945a08f21c7adbeb5
gapson <hgapson@gmail.com>
2024-07-02 15:18:14 +1200

    adding logo

--- client/components/Logo.tsx
+++ client/components/Logo.tsx
@@ -3,7 +3,7 @@
 function Logo() {
   return (
     <div className="flex items-center space-x-2">
-      <img src="/path/to/your/logo.png" alt="Logo" className="h-8" />
+      <img src="/images/logo.png" alt="Logo" className="h-8" />
     </div>
   )
 }
