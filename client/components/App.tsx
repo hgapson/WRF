9df3b969239cc1e14a9a01d7b39da0835ef079dc
gapson <hgapson@gmail.com>
2024-07-05 15:45:39 +1200

    adding home info

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -2,22 +2,24 @@
 import Header from './Header'
 import Footer from './Footer'
 import Background from './Background' // Adjust the path as per your project structure
+import '../styles/main.scss'
 
 const App = () => {
   return (
     <div className="app">
-      <div className="heading">
-        <Header />
-      </div>
-      <div className="background">
-        <Background />
-      </div>
+      <main className="main-contener">
+        <div className="heading">
+          <Header />
+        </div>
+        <div className="background">
+          <Background />
+        </div>
 
-      {/* Your main content here */}
-
-      <div className="footer">
-        <Footer />
-      </div>
+        {/* Your main content here */}
+        <div className="footer">
+          <Footer />
+        </div>
+      </main>
     </div>
   )
 }
--- client/components/Background.tsx
+++ client/components/Background.tsx
@@ -1,30 +1,37 @@
-//import React from 'react'
+import React from 'react'
 import cover from '../public/cover.jpg'
-import '../styles/main.scss'
+import '../styles/main.scss' // Make sure the path is correct
 
 const Background = () => {
   return (
-    <div className="main-content">
-      <section className="home section">
-        <div className="container">
-          <div className="row">
-            <div className="home-info padd-15">
-              <h3 className="hello">
-                Welcome to <span className="name"> WRF</span>
+    <div className="main-content flex min-h-screen items-center justify-center bg-blue-100">
+      <section className="home section w-full px-4">
+        <div className="container mx-auto">
+          <div className="row flex flex-col-reverse items-center justify-between lg:flex-row">
+            <div className="home-info flex-1 p-4 text-center lg:order-1 lg:mr-8 lg:text-left">
+              <h3 className="hello text-xl font-bold lg:text-2xl xl:text-3xl">
+                Welcome to <span className="name">WRF</span>
               </h3>
-              <h3 className="my-profession">
+              <h3 className="my-profession text-lg lg:text-xl xl:text-2xl">
                 <span className="typing">Waikato Refugee Forum</span>
               </h3>
-              <p className="font-serif">
-                Bring together the voice of all refugee communities settle in
+              <p className="mt-4 font-serif text-sm lg:text-base xl:text-lg">
+                Bring together the voice of all refugee communities settled in
                 the Waikato Region
               </p>
-              <a href="#contact" className="btn hire-me">
+              <a
+                href="#contact"
+                className="btn hire-me mt-4 inline-block rounded bg-blue-500 px-4 py-2 text-white"
+              >
                 Contact Us
               </a>
             </div>
-            <div className="home-image padd-15">
-              <img src={cover} alt="background" />
+            <div className="home-image flex flex-1 justify-center p-4 lg:justify-end">
+              <img
+                src={cover}
+                alt="background"
+                className="w-full max-w-lg object-cover lg:max-w-xl xl:max-w-2xl"
+              />
             </div>
           </div>
         </div>
