374378a66ebab2f44eea7657e757afbd3276ffa5
gapson <hgapson@gmail.com>
2024-08-14 16:26:54 +1200

    adding imges and background on contact page

--- client/components/ContactPage/ContactSection.tsx
+++ client/components/ContactPage/ContactSection.tsx
@@ -1,5 +1,3 @@
-// src/components/ContactPage/Map.tsx
-
 import React from 'react'
 import { FaMapMarkerAlt, FaPhone, FaEnvelope } from 'react-icons/fa'
 import mapImage from '../public/map-location.png' // Replace with your map image
@@ -11,20 +9,31 @@ const ContactSection: React.FC = () => {
       <div className="flex-1 md:pr-8">
         {/* Contact Information */}
         <div className="mb-6 space-y-4">
-          <div className="flex items-center space-x-4">
+          <a
+            href="https://maps.google.com/?q=46G+Boundary+Road,+Claudelands,+Hamilton+3214"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex items-center space-x-4 hover:text-blue-600"
+          >
             <FaMapMarkerAlt size={30} className="text-blue-500" />
             <p className="text-lg">
               46G Boundary Road, Claudelands, Hamilton 3214
             </p>
-          </div>
-          <div className="flex items-center space-x-4">
+          </a>
+          <a
+            href="tel:+6471234567"
+            className="flex items-center space-x-4 hover:text-green-600"
+          >
             <FaPhone size={24} className="text-green-500" />
             <p className="text-lg">+64 7 123 4567</p>
-          </div>
-          <div className="flex items-center space-x-4">
+          </a>
+          <a
+            href="mailto:info@example.com"
+            className="flex items-center space-x-4 hover:text-red-600"
+          >
             <FaEnvelope size={24} className="text-red-500" />
             <p className="text-lg">info@example.com</p>
-          </div>
+          </a>
         </div>
       </div>
 
