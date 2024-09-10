faf1e4deefca19ffb1dd17af89bda360f53977bb
gapson <hgapson@gmail.com>
2024-09-10 13:24:15 +1200

    adding Error boundary

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { Suspense } from 'react'
 import { RouterProvider } from 'react-router-dom'
-import router from './routes' // Ensure the path is correct
+import router from './routes'
+import ErrorBoundary from './ErrorBoundary'
 
 const App = () => {
-  return <RouterProvider router={router} />
+  return (
+    <ErrorBoundary>
+      <Suspense fallback={<div>Loading...</div>}>
+        <RouterProvider router={router} />
+      </Suspense>
+    </ErrorBoundary>
+  )
 }
 
 export default App
